1091425f587a04a8a9461d23395779a30a2d2ab6
Robin Hughes <robin22391@hotmail.com>
2018-12-18 18:12:38 +0000

    adds changes to sql runner

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -5,7 +5,7 @@ class SqlRunner
   def self.run( sql, values = [] )
     begin
       # db = PG.connect({ dbname: 'spending_tracker', host: 'localhost' }) //use for local db not heroku
-      PG.connect( {dbname: 'dbu9iktnfng3fd',
+      db = PG.connect( {dbname: 'dbu9iktnfng3fd',
         host: 'ec2-54-217-223-175.eu-west-1.compute.amazonaws.com',
         port: 5432, user: 'fhxbfksksvpmeo', password: '90503da12a7e20c501f6defbdacc675f2a62aeb08e9b5aef9cfabc57d33a4d55'})
         db.prepare("query", sql)
